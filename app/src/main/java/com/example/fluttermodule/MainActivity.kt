005c41c82e588a90fabe2a404a423771a9b7cef7
fida1989 <fida.muntaseer.1989@gmail.com>
2020-09-02 22:28:33 +0600

    Various Updates

--- app/src/main/java/com/example/fluttermodule/MainActivity.kt
+++ app/src/main/java/com/example/fluttermodule/MainActivity.kt
@@ -4,12 +4,40 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+import io.flutter.plugin.common.MethodChannel
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var flutterEngine: FlutterEngine
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Instantiate a FlutterEngine.
+        flutterEngine = FlutterEngine(this)
+
+        // Start executing Dart code to pre-warm the FlutterEngine.
+        flutterEngine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+
+        // Cache the FlutterEngine to be used by FlutterActivity.
+        FlutterEngineCache
+            .getInstance()
+            .put("my_engine_id", flutterEngine)
+
+        // Pass message to flutter module
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            "demo_channel"
+        )
+            .invokeMethod("fromHostToClient", "hello from android")
+
+        // Button click listener
         findViewById<Button>(R.id.button1).setOnClickListener {
             startActivity(
                 FlutterActivity
@@ -18,4 +46,5 @@ class MainActivity : AppCompatActivity() {
             )
         }
     }
+
 }
